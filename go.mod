5833ff7dc2f7d475d1887bf7cb78d715014d7ee5
Petter Kvalvaag <pmkv@equinor.com>
2022-09-23 13:53:24 +0200

    Simple API for forwarding POSTs to event hub

---
+++ go.mod
@@ -0,0 +1,25 @@
+module equinor/snykEventHandler
+
+go 1.18
+
+require (
+	github.com/Azure/azure-amqp-common-go v1.1.4
+	github.com/Azure/azure-event-hubs-go v1.3.1
+	github.com/go-kit/kit v0.12.0
+)
+
+require (
+	github.com/Azure/azure-sdk-for-go v21.3.0+incompatible // indirect
+	github.com/Azure/go-autorest v11.1.1+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/jpillora/backoff v0.0.0-20180909062703-3050d21c67d7 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	golang.org/x/crypto v0.0.0-20210915214749-c084706c2272 // indirect
+	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf // indirect
+	pack.ag/amqp v0.11.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,92 @@
+// SnykEventService provides operations for snyk events.
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	aad "github.com/Azure/azure-amqp-common-go/aad"
+	eventhubs "github.com/Azure/azure-event-hubs-go"
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+type SnykEvent struct {
+	TimeStamp (time.Time)
+	Value     (map[string]interface{})
+}
+
+type SnykEventService interface {
+	Store(SnykEvent) (string, error)
+}
+
+type snykEventService struct{}
+
+func (snykEventService) Store(event SnykEvent) (string, error) {
+	return event.TimeStamp.String(), nil
+}
+
+type storeRequest struct {
+	Event (map[string]interface{})
+}
+
+type storeResponse struct {
+	Event (map[string]interface{})
+}
+
+func makeStoreEndpoint(ses SnykEventService) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		req := request.(storeRequest)
+		snykEvent := SnykEvent{time.Now(), req.Event}
+		eventValue, err := json.Marshal(snykEvent)
+		tokenProvider, err := aad.NewJWTProvider(aad.JWTProviderWithEnvironmentVars())
+		if err != nil {
+			log.Fatalf("failed to configure AAD JWT provider: %s\n", err)
+		}
+		hub, err := eventhubs.NewHub("snyk-events", "snyk-events", tokenProvider)
+		if err != nil {
+			log.Fatalf("failed to get hub %s\n", err)
+		}
+		// ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		ctx, cancel := context.WithCancel(context.Background())
+		event := eventhubs.NewEventFromString(string(eventValue))
+		hub.Send(ctx, event)
+		defer cancel()
+		defer hub.Close(ctx)
+		err = hub.Close(context.Background())
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		return storeResponse{req.Event}, nil
+	}
+}
+
+func main() {
+	ses := snykEventService{}
+
+	storeSnykEventHandler := httptransport.NewServer(
+		makeStoreEndpoint(ses),
+		decodeStoreRequest,
+		encodeStoreResponse,
+	)
+
+	http.Handle("/store", storeSnykEventHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func decodeStoreRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var request storeRequest
+	if err := json.NewDecoder(r.Body).Decode(&request.Event); err != nil {
+		return nil, err
+	}
+	return request, nil
+}
+
+func encodeStoreResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
