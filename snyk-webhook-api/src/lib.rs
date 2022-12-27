9cdcd3097511fd2687d2f613442d63ff81f91ad3
Petter Kvalvaag <pmkv@equinor.com>
2022-12-28 00:36:49 +0100

    Put dotenv on hold

--- snyk-webhook-api/src/lib.rs
+++ snyk-webhook-api/src/lib.rs
@@ -5,7 +5,7 @@ use std::env;
 
 
 pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
+    // dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
