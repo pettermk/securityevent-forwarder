c54b464b4330f0ab217256ae1975bf2ee3126433
Petter Kvalvaag <pmkv@equinor.com>
2023-01-06 14:49:27 +0100

    Reintroduce dotenvy
    Simpler for local development, but only use environments
    in prod

--- snyk-webhook-api/src/lib.rs
+++ snyk-webhook-api/src/lib.rs
@@ -2,10 +2,14 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
 use std::env;
+use std::path::PathBuf;
 
 
 pub fn establish_connection() -> PgConnection {
-    // dotenv().ok();
+    match dotenv() {
+        Ok(result) => result,
+        Err(_) => PathBuf::new(),
+    };
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
